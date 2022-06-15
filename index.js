3a8e013a4005c89771101e27c4d2b85f0d56918c
root <root@HomeLPT-001.localdomain>
2022-06-16 00:44:45 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+// This function moves the dodger left
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+// This function moves the dodger right
+function moveDodgerRight() {
+    const leftNum = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNum, 10);
+
+    if (left <= 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+// Adds the eventlistener for the keydown events to the DOM
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+
+    else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
